374f4741562d4cdceb2a7584635d6241d28a8501
503 <503@academicos.cesde.edu.co>
2023-03-30 19:41:31 -0500

    Prueba

--- App.js
+++ App.js
@@ -1,11 +1,63 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+ssscdsdfsadasdcgsacdgasdsfdasfdassssss
+savdghasfgdfsagdfsa
+asdasdas
+asdasdasasd
+asdasdsadsad
+sadbvsabdv
+hagdsh
+savdghasfgdfsagdfsadfs
+useDeferredValuefds
+falsesdf
+sadbvsabdvfds
+FileSystemDirectoryEntryfd
+ssscdsdfsadasdcgsacdgasdsfdasfdassssssdf
+ssscdsdfsadasdcgsacdgasdsfdasfdassssssds
+FileSystemDirectoryEntryf
+DevSettingsfds
+fds
 
+import { DevSettings, StyleSheet, Text, View } from 'react-native';
+import { TextInput } from 'react-native-paper';
+
+// libreria de validaciones
+
+import { useForm, Controller } from "react-hook-form";
+import { useDeferredValue } from 'react'
 export default function App() {
+
+  // definiciones del formulario con sus respetivos  estados,errores, etc 
+  
+    const { control, handleSubmit,reset, formState: { errors } } = useForm({
+      defaultValues: {
+        fullname: '',
+        email: '',
+        phone: '',
+        password: '',
+        age: '',
+
+      }
+    });
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Controller
+        control={control}
+        rules={{
+         required: true,
+        }}
+        render={({ field: { onChange, onBlur, value } }) => (
+          <TextInput
+         label="Nombre completo"
+         mode='outlined'
+         left={<TextInput.Icon icon="account"/>}
+            onBlur={onBlur}
+            onChangeText={onChange}
+            value={value}
+          />
+        )}
+        name="fullname"
+      />
+      {errors.fullname && <Text>This is required.</Text>}
+    
     </View>
   );
 }
